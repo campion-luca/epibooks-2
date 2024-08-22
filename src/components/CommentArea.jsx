7d295e81d29f837eb304717505b4954bfa945114
campion-luca <campionluca3@gmail.com>
2024-08-22 16:38:41 +0200

    CommentArea..

--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -1,42 +1,38 @@
 import { Component } from "react";
 import { ListGroup } from "react-bootstrap";
 
-
 class CommentArea extends Component {
   state = {
     comment: false,
-    showComment: []
+    showComment: [],
   };
 
   fetchBook = () => {
     fetch("https://striveschool-api.herokuapp.com/api/comments/")
-    .then((response) => {
+      .then((response) => {
         if (response.ok) {
-            return response.json()
+          return response.json();
         } else {
-            throw new Error("la chiamata non è andata a buon fine")
+          throw new Error("la chiamata non è andata a buon fine");
         }
-    })
-    .then((arrayOfComments) => {
+      })
+      .then((arrayOfComments) => {
         this.setState({
-            showComment: arrayOfComments
-        })
-    })
-    .catch((err) => {
-        console.log("ERRORE NEL RECUPERO DATI", err)
-    })
-}
-
+          showComment: arrayOfComments,
+        });
+      })
+      .catch((err) => {
+        console.log("ERRORE NEL RECUPERO DATI", err);
+      });
+  };
 
   render() {
-    return <ListGroup.Item>COMMENTO BLA BLA BLA</ListGroup.Item>;
+    this.state.showComment.map((book) => {
+        return (
+            <ListGroup.item> ciao ciao </ListGroup.item>
+        )
+    })
   }
 }
-
-
-
-
-
-
 
 export default CommentArea;
